89b5139f85d1b307f42dae1d3d224d71af411635
Enyeltov <enyeltov.16@gmail.com>
2022-06-03 09:42:46 +0200

    Cambiado Grid a Flex

--- renderer/components/Layout/Layout.jsx
+++ renderer/components/Layout/Layout.jsx
@@ -10,7 +10,7 @@ export default function Layout({ children, title }) {
             <main className="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]">
                 <Header title={title} />
                 <div className="px-6 pt-6 2xl:container">
-                    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+                    <div className="flex flex-col">
                         {children}
                     </div>
                 </div>
